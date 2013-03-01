a7bdd33eeb70eef7bfe883fec9873f125c33206b
Alan Skorkin <askorkin@gmail.com>
2013-03-02 00:53:04 +1100

    No more curses

--- lib/escort/formatter/terminal.rb
+++ lib/escort/formatter/terminal.rb
@@ -1,5 +1,3 @@
-require 'curses'
-
 module Escort
   module Formatter
     class Terminal
@@ -7,15 +5,26 @@ module Escort
 
       class << self
         def width
-          screen_size = self::DEFAULT_WIDTH
-          Curses.init_screen
-          begin
-            screen_size = Curses.cols
-          ensure
-            Curses.close_screen
-          end
-          screen_size
+          tput_width
         end
+
+        private
+
+        def tput_width
+          `/usr/bin/env tput cols`.to_i
+        end
+
+        #def curses_width
+          #require 'curses'
+          #screen_size = self::DEFAULT_WIDTH
+          #Curses.init_screen
+          #begin
+            #screen_size = Curses.cols
+          #ensure
+            #Curses.close_screen
+          #end
+          #screen_size
+        #end
       end
     end
   end
